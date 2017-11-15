acbd986799955248e1667e3dc595bcd1b54037f9
Hakeem Buchanan <hakeemsbuchanan@outlook.com>
2017-11-15 15:45:05 -0500

    Update
    Adding the string and making the display available.

--- Project3/Reservation.cpp
+++ Project3/Reservation.cpp
@@ -1,3 +1,8 @@
+/* Programmer   	: Hakeem Buchanan
+ * File Name   		:Reservation_Display
+ * Course     		: Computational Problem Solving II - CPET
+ * Date Created 	: 11/8/2017
+*/
 #include <string>
 #include <iostream>
 using namespace std;
@@ -6,29 +11,29 @@ int main()
 {
 	bool SeatTaken;
 	char vehicle;
-	int max_value;
+	string max_value;
 string schedule[2][19]=
 {
 {" ","800","830","900","930","1000","1030","1100","1130","1200","1300","1330","1400","1430","1500","1530","1600","1630","1700"},
 {" ","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x"}};
 
 const int times[19]={800,830,900,930,1000,1030,1100,1130,1200,1300,1330,1400,1430,1500,1530,1600,1630,1700};
-char availability[19]{'X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X'};
+string availability[19]{"X","X","X","X","X","X","X","X","X","X","X","X","X","X","X","X","X","X","X"};
 
 switch(vehicle){
 case 'm':
 case 'M':
-	max_value=1;
+	max_value="1";
 	break;
 
 case 'C':
 case 'c':
-	max_value=2;
+	max_value="2";
 	break;
 
 case 'V':
 case 'v':
-	max_value=4;
+	max_value="4";
 	break;
 }
 
@@ -42,13 +47,21 @@ case 'v':
 
  for(int j=0;j<18;j++)
  {
-	 cout<<"  "<<times[j];
-	 cout<<"  "<<availability[j];
-	 }
- }
+	 cout<<"\n  "<<times[j]<<":";
+
+ for (int h =0;h<18;h++){
+
+	 cout<<"  "<<availability[h];
 
 
-};
+ };
+
+ };
+
+ };
+
+
+
 
 
 
