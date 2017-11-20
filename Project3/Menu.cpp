513475432b345aa76b8e2d8939f5552158a02038
seanw238 <33638495+seanw238@users.noreply.github.com>
2017-11-19 23:35:53 -0500

    Comments - Sean

--- Project3/Menu.cpp
+++ Project3/Menu.cpp
@@ -1,9 +1,9 @@
-/*
- * Main.cpp
- *
- *  Created on: Nov 19, 2017
- *      Author: hakee
- */
+/* Programmer   	: Hakeem Buchanan
+ * File Name   		: Menu.cpp
+ * Course     		: Computational Problem Solving II - CPET
+ * Date Created 	: 11/19/2017
+*/
+
 #include <iostream>
 #include <string>
 #include "Reservation.h"
@@ -16,8 +16,8 @@ using namespace std;
 int main(){
 	int Menu_Options;
 	char Reserve_Code;
-	bool Quit=false;
-   Driver_SYS();
+	bool Quit=false; // Quit is currently false
+   Driver_SYS(); // calls Driver_SYS function from Driver_sys header file
    do {
 	cout<<"\nHello Dispatch Officer, Please input the function you would like to perform from the menu list."<<endl;
 	cout<<"\nMain Menu\n";
@@ -26,45 +26,45 @@ int main(){
 	cout<<"3- Search for Reservation(Must haver Reservation Code)\n ";
 	cout<<"4- Display All Time-Slots\n";
 	cout<<"5- Summary of all driver information and then quit the application\n";
-	cin>>Menu_Options;
-
-
+	cin>>Menu_Options; // input value of 1 to 5 to access menu options
 
 	switch(Menu_Options){
-	case 1:
-		create();
+	case 1: // Create Reservation case
+		create(); // calls for create function in the Dispatch header file
 		break;
-	case 2:
+
+	case 2: // Delete Reservation case
 		cout<<"\nDo you have a Reservation Code? Y/N?\n";
 		cin>>Reserve_Code;
-		if(Reserve_Code=='n'||Reserve_Code=='N'){
+		if(Reserve_Code=='n'||Reserve_Code=='N')
+		{
 			cout<<"Please get a Reserve Code before continuing."<<endl;
-
 		}
 
 		break;
-	case 3:
+
+	case 3: // Search for Reservation case
 		cout<<"\nDo you have a Reservation Code? Y/N?\n";
 		cin>>Reserve_Code;
-		if(Reserve_Code=='n'||Reserve_Code=='N'){
-					cout<<"Please get a Reserve Code before continuing."<<endl;
-				}
+		if(Reserve_Code=='n'||Reserve_Code=='N')
+		{
+			cout<<"Please get a Reserve Code before continuing."<<endl;
+		}
 		break;
-	case 4:
+
+	case 4: // Display case
 		display();
 		break;
-	case 5:
+
+	case 5: // Summary and end case
 		cout << "\nThis is a summary of all the drivers information. ";
-		display();
+		display(); // calls for display function in the Dispatch header file
 		cout<<"\n\nThank you goodbye."<<endl;
-		Quit=true;
+		Quit=true; // Quit is now true
 		break;
 
 	}
-}while(Quit==false);
+   }while(Quit==false); // do while condition of Quit is false, end when Quit is true
 
-
-return 0;
+   return 0;
 }
-
-
